2f5f56e81cfab881d8776932ea4b78870a114fff
kubilaycicek <kubilaycicek0693@gmail.com>
2021-04-28 22:29:06 +0300

    Exchange Service init

---
+++ currency-exchange-service/src/main/java/com/kubilaycicek/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,23 @@
+package com.kubilaycicek.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
+    }
+}
